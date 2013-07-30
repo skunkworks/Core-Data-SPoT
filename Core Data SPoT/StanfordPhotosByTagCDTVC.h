05b90564f391b693d1f4c1e90a0db0d18f7aa39b
Richard Shin <skunkworks@gmail.com>
2013-07-30 13:44:47 -0700

    Starting Stanford Photos By Tag

---
+++ Core Data SPoT/StanfordPhotosByTagCDTVC.h
@@ -0,0 +1,13 @@
+//
+//  StanfordPhotosByTagCDTVC.h
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/30/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import "TagsCDTVC.h"
+
+@interface StanfordPhotosByTagCDTVC : TagsCDTVC
+
+@end
---
+++ Core Data SPoT/StanfordPhotosByTagCDTVC.m
@@ -0,0 +1,38 @@
+//
+//  StanfordPhotosByTagCDTVC.m
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/30/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import "StanfordPhotosByTagCDTVC.h"
+
+@interface StanfordPhotosByTagCDTVC ()
+
+@end
+
+@implementation StanfordPhotosByTagCDTVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
