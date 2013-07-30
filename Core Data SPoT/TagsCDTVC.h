6cbe7b7d1de265ebd3c70b202add56528cb8f680
Richard Shin <skunkworks@gmail.com>
2013-07-30 13:42:46 -0700

    Tags CDTVC

---
+++ Core Data SPoT/TagsCDTVC.h
@@ -0,0 +1,13 @@
+//
+//  TagsCDTVC.h
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/30/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface TagsCDTVC : CoreDataTableViewController
+
+@end
---
+++ Core Data SPoT/TagsCDTVC.m
@@ -0,0 +1,17 @@
+//
+//  TagsCDTVC.m
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/30/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import "TagsCDTVC.h"
+
+@interface TagsCDTVC ()
+
+@end
+
+@implementation TagsCDTVC
+
+@end
