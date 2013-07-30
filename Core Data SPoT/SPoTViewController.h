982b690dc8a9a786044dfc5f3c8447025e6b6fb7
Richard Shin <skunkworks@gmail.com>
2013-07-29 21:50:03 -0700

    Initial Commit

---
+++ Core Data SPoT/SPoTViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPoTViewController.h
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/29/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPoTViewController : UIViewController
+
+@end
---
+++ Core Data SPoT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Core Data SPoT
+//
+//  Created by Richard Shin on 7/29/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPoTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPoTAppDelegate class]));
+    }
+}
